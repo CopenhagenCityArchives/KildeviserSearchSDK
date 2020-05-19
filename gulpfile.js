267541bcef40ddaf2250cfc129d5cdb8cb47e9df
jfeo <jens@feodor.dk>
2020-05-19 14:46:08 +0200

    Update gulpfile.js and add new select2 lib
    - Add production and development options
    - a buildDev function/cmd, and use it in watcher
    - Formatting and comments
    
    - Add select2 4.0.1 and use unminified for dev build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-// Dependencies:
+///////////////////////////////////////////////////////////////////////////////
+// Load dependencies
+///////////////////////////////////////////////////////////////////////////////
 
 // Gulp
 const { src, dest, series, parallel, watch } = require('gulp');
@@ -12,22 +14,48 @@ var concat = require('gulp-concat');
 // Webserver with LiveReload
 var connect = require( 'gulp-connect');
 
-//Options
-var opt = {};
+///////////////////////////////////////////////////////////////////////////////
+// Options
+///////////////////////////////////////////////////////////////////////////////
 
+// Production
+var prod = {}
+prod.dest = "dist";
+prod.fileName = "KildeviserSearchSDK.min.js";
+prod.src = [
+    "src/models/*.js",
+    "lib/select2/js/select2.min.js",
+    "lib/mithril/mithril.min.js",
+    "src/app.js",
+    "!" + prod.fileName
+];
 
-opt.dest = "dist";
-opt.prod = "";
-opt.concatFileName = "concat.js";
-opt.minifyFileName = "KildeviserSearchSDK.min.js";
-opt.src = ["src/models/*.js","lib/select2/select2.min.js","lib/mithril/mithril.min.js","src/app.js","!" + opt.minifyFileName];
+// Development
+var dev = {}
+dev.dest = "dist";
+dev.fileName = "KildeviserSearchSDK.js";
+dev.src = [
+    "src/models/*.js",
+    "lib/select2/js/select2.js",
+    "lib/mithril/mithril.js",
+    "src/app.js",
+    "!" + dev.fileName
+];
+///////////////////////////////////////////////////////////////////////////////
+// Tasks
+///////////////////////////////////////////////////////////////////////////////
 
+// Concats without minifying for development
+function buildDev() {
+    return src(dev.src)
+        .pipe(concat(dev.fileName))
+        .pipe(dest(dev.dest))
+}
 
-//Tasks:
-
-//Minifies Javascript files
-function build(){
-    return src(opt.src)
+// Concats and minifies for production
+function build() {
+    return src(prod.src)
+        .pipe(concat(prod.fileName))
         .pipe(minifier({
             minify: true,
             collapseWhitespace: true,
@@ -35,30 +63,37 @@ function build(){
             minifyJS: true,
             minifyCSS: true
         }))
-        .pipe(concat(opt.minifyFileName))
-        .pipe(dest(opt.dest));
+        .pipe(dest(prod.dest));
 }
 
+// Copies assets into dist
 function copyAssets(){
     return src('./lib/select2/select2.css')
         .pipe(dest('./dist/'));
 }
 
-function startWebserver(cb){
-    
+// Starts a development webserver
+function startWebserver(callback){
     connect.server({
         root: './',
         livereload: true,
         fallback: '/examples/example.html'
-      });
+    });
 
-      cb();
+    callback();
 }
 
+// Returns a watcher function
 function watcher(){
-    return watch(['src/**/*.*'], { delay: 500 }, build);
+    return watch(['src/**/*.*'], { delay: 500 }, buildDev);
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Export gulp commands
+///////////////////////////////////////////////////////////////////////////////
 
 exports.build = series(build, copyAssets);
+exports.buildDev = series(buildDev, copyAssets);
+
 exports.serve = startWebserver;
 exports.watch = series(startWebserver, watcher);
