44bfed85157dbd3fec3b2a2369364c96f35c5aac
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2025-01-31 09:35:20 +0100

    Profiles: update kildeviser-staging url

--- profile/kbharkiv-staging.js
+++ profile/kbharkiv-staging.js
@@ -1,6 +1,6 @@
 var ProfileConfiguration = function() {
     return {
-        kildeviserUrl: "https://dev-kildeviser.kbharkiv.dk/",
+        kildeviserUrl: "https://kildeviser-staging.kbharkiv.dk/",
         select2Theme: 'bootstrap4',
         apiUrl: 'https://api-staging.kbharkiv.dk/'
     }
